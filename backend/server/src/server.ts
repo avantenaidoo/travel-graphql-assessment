85ce9d3d6e7638c6a8ed898c710f6a28e440e8d2
avantenaidoo <60886136+avantenaidoo@users.noreply.github.com>
2025-11-16 11:26:40 +0200

    Update existing server.ts code

--- backend/server/src/server.ts
+++ backend/server/src/server.ts
@@ -1,62 +1,22 @@
-import express from "express";
-import type { Request, Response } from "express";
-import type { NextFunction } from "express";
-import cors from "cors";
-import { logErrors, logEvents, logRequests } from "./middleware/logEvents.js";
-import apiRoutes from "./routes/apiRoutes.js";
-import { config } from "./config/config.js";
-import limiter from "./middleware/rateLimiter.js";
-import cron from "node-cron";
-import { cleanupAllLogs } from "./utils/cleanupLogs.js";
+import 'dotenv/config';
+import express from 'express';
+import { ApolloServer } from 'apollo-server-express';
+import { typeDefs } from './graphql/schema';
+import { resolvers } from './graphql/resolvers';
+import cors from 'cors';
 
-// Create the Express app
 const app = express();
-const PORT = config.port || 3000;
+app.use(cors());
 
-app.set('trust proxy', 1);
+async function startServer() {
+  const server = new ApolloServer({ typeDefs, resolvers });
+  await server.start();
+  server.applyMiddleware({ app });
 
-app.use('/api/', limiter);
-
-// Middleware to handle JSON
-app.use(express.json());
-
-// Log events
-app.use(async (request: Request, response: Response, next: NextFunction) => {
-  response.on('finish', async () => {
-    await logEvents('Received request for weather data.');
-
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () => {
+    console.log(`Server ready at http://localhost:${PORT}${server.graphqlPath}`);
   });
-  next();
-});
+}
 
-// Enable CORS for specific origins
-app.use(cors({
-  origin: ['http://localhost:5173','https://avantenaidoo.github.io'],
-  methods: 'GET, HEAD, PUT, PATCH, POST, DELETE',
-  allowedHeaders: 'Content-Type, Authorization',
-  maxAge: 600
-}));
-
-// Custom Middleware logger
-app.use(logRequests);
-
-
-// Use the API routes
-app.use("/api", apiRoutes);
-
-app.get("/", (request: Request, response: Response) => {
-  response.send("Welcome to the Weather App!");
-});
-
-// log errors
-app.all('*', logErrors);
-
-cron.schedule('* * * * *', async () => {
-  console.log('Starting log cleanup job...');
-  await cleanupAllLogs();
-  console.log('Log cleanup job completed.');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+startServer().catch(console.error);
